0b06c023848a27feabc2619786833bc494772889
Zietieflr <lrmcguire93@gmail.com>
2020-09-15 15:05:43 -0600

    Update validations. Update sign-up returns.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if !@user.valid?
+      render json: { error: 'Username not available' }, status: :unauthorized
     else
-      render json: @user.errors, status: :unprocessable_entity
+      @user.save
+      payload = {
+        user_id: @user.id
+      }
+      secret = Rails.application.secret_key_base
+      token = JWT.encode payload, secret
+
+      render json: { token: token }, status: :created
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :schedules
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
 end
